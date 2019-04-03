34c7a944e53bfedf9797c2693603e199d8780372
damian <593700742@qq.com>
2019-04-03 11:25:03 +0800

    v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <transition name="fade" mode="out-in">
-      <router-view/>
-    </transition>
-    <el-dialog title="住院药房管理子系统登录" :visible.sync="dialogVisible" center :append-to-body='true' :lock-scroll="false" width="30%">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>
+          <transition name="fade" mode="out-in">
+            <router-view/>
+          </transition>
+        </el-main>
+      </el-container>
+      <el-footer>Footer</el-footer>
+    </el-container>
+    <el-dialog class="register" title="住院药房管理子系统登录" :visible.sync="dialogVisible"
+               center :append-to-body='true' :lock-scroll="false"
+               width="30%">
       <Login/>
     </el-dialog>
-
-    <!--    <dialog ref="dialog"/>-->
   </div>
 </template>
 
 <script>
-import Dialog from './components/Dialog'
 import Login from './components/Login'
 
 export default {
@@ -21,7 +29,7 @@ export default {
       dialogVisible: true
     }
   },
-  components: {Login, Dialog},
+  components: {Login},
   name: 'App',
   methods: {
     // submitForm () {
@@ -46,5 +54,43 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+  }
+
+  .register:only-child {
+    min-width: 350px;
+  }
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
   }
 </style>
