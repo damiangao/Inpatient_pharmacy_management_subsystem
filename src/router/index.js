9b0f12e8816b691f60e55af45ffe266a0281819e
何睿扬 <heruiyang@outlook.com>
2019-04-04 17:31:59 +0800

    outStock
    出库

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import TestPage1 from '@/components/TestPage1'
 import TestPage2 from '@/components/TestPage2'
+import OutStock from '@/components/OutStock'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/test2',
       name: 'TestPage2',
       component: TestPage2
+    },
+    {
+      path: '/outStock',
+      name: 'OutStock',
+      component: OutStock
     }
   ]
 })
